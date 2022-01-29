cc81f0f5241488f4691a87a9af55678a842258d4
cmoten <cmoten@gmail.com>
2022-01-29 16:02:06 +0100

    completed initial updates of user interface

--- mean-app/src/app/posts/post-create/post-create.component.ts
+++ mean-app/src/app/posts/post-create/post-create.component.ts
@@ -1,13 +1,23 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-create',
-  templateUrl: './post-create.component.html'
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css'],
 })
 export class PostCreateComponent {
-  enteredValue = '';
-  newPost = 'No Content';
-  onAddPost() {
-    this.newPost = this.enteredValue;
+  enteredTitle = '';
+  enteredContent = '';
+
+  constructor(public postsService: PostsService) {}
+
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    this.postsService.addPost(form.value.title, form.value.content);
+    form.resetForm();
   }
 }
