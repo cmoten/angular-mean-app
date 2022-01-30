1d6da33ce72f43f9458c2b6407b31b8abdbc9f3c
cmoten <cmoten@gmail.com>
2022-01-30 15:47:03 +0100

    integrated Mongodb

--- mean-app/src/app/posts/posts.service.ts
+++ mean-app/src/app/posts/posts.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Post } from './post.model';
 
 @Injectable({ providedIn: 'root' })
@@ -12,9 +13,20 @@ export class PostsService {
 
   getPosts() {
     this.http
-      .get<{ message: string; posts: Post[] }>('//localhost:3000/api/posts')
-      .subscribe((postData) => {
-        this.posts = postData.posts;
+      .get<{ message: string; posts: any }>('//localhost:3000/api/posts')
+      .pipe(
+        map((postData) => {
+          return postData.posts.map((post) => {
+            return {
+              title: post.title,
+              content: post.content,
+              id: post._id,
+            };
+          });
+        })
+      )
+      .subscribe((transformedPosts) => {
+        this.posts = transformedPosts;
         this.postsUpdated.next([...this.posts]);
       });
   }
@@ -26,11 +38,23 @@ export class PostsService {
   addPost(title: string, content: string) {
     const post: Post = { id: null, title: title, content: content };
     this.http
-      .post<{ message: string }>('//localhost:3000/api/posts', post)
+      .post<{ message: string; postID: string }>(
+        '//localhost:3000/api/posts',
+        post
+      )
       .subscribe((responseData) => {
-        console.log(responseData.message);
+        const postID = responseData.postID;
+        post.id = postID;
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
       });
+  }
+
+  deletePost(postID: string) {
+    this.http.delete('//localhost:3000/api/posts/' + postID).subscribe(() => {
+      const updatedPosts = this.posts.filter((post) => post.id !== postID);
+      this.posts = updatedPosts;
+      this.postsUpdated.next([...this.posts]);
+    });
   }
 }
