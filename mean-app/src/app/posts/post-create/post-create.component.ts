769afb0c6ba0851000f837c36dde2e7d2a5be3b7
cmoten <cmoten@gmail.com>
2022-01-31 17:25:13 +0100

    updating routing module

--- mean-app/src/app/posts/post-create/post-create.component.ts
+++ mean-app/src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
 
 @Component({
@@ -7,17 +9,44 @@ import { PostsService } from '../posts.service';
   templateUrl: './post-create.component.html',
   styleUrls: ['./post-create.component.css'],
 })
-export class PostCreateComponent {
+export class PostCreateComponent implements OnInit {
   enteredTitle = '';
   enteredContent = '';
+  post: Post;
+  private mode = 'create';
+  private postID: string;
 
-  constructor(public postsService: PostsService) {}
+  constructor(
+    public postsService: PostsService,
+    public route: ActivatedRoute
+  ) {}
 
-  onAddPost(form: NgForm) {
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has('postID')) {
+        this.mode = 'edit';
+        this.postID = paramMap.get('postID');
+        this.post = this.postsService.getPost(this.postID);
+      } else {
+        this.mode = 'create';
+        this.postID = null;
+      }
+    });
+  }
+
+  onSavePost(form: NgForm) {
     if (form.invalid) {
       return;
     }
-    this.postsService.addPost(form.value.title, form.value.content);
+    if (this.mode === 'create') {
+      this.postsService.addPost(form.value.title, form.value.content);
+    } else {
+      this.postsService.updatePost(
+        this.postID,
+        form.value.title,
+        form.value.content
+      );
+    }
     form.resetForm();
   }
 }
